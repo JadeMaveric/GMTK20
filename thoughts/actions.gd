4e9e717da14d0f6483205c2c5a41347c3bed1404
JuliusAlphonso <JadeMaveric@users.noreply.github.com>
2020-07-11 16:54:53 +0530

    Add brownian motion
    Thoughts now move randomly, making them tougher to track and click.
    They also move when the mouse goes over them, increasing the difficulty
    of tracking and clicking.
    
    Signed-off-by: JuliusAlphonso <JadeMaveric@users.noreply.github.com>

--- thoughts/actions.gd
+++ thoughts/actions.gd
@@ -1,10 +1,10 @@
 extends RigidBody2D
-
+class_name Thought
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export(int) var speed = 10
+export(int) var speed = 110
 
 var ThoughtNode = load("res://thoughts/Thought.tscn")
 
@@ -27,7 +27,25 @@ func _input_event(viewport, event, shape_idx):
 	elif event.is_action_pressed("gp_save"):
 		# Slide left, generate child on right
 		var clone = ThoughtNode.instance()
-		clone.position = position + rand_vector()
-		position = position + rand_vector()
+		var clone_dir = rand_vector()
+		clone.set_linear_velocity((clone_dir-position).normalized() * 110)
+		clone.position = position + clone_dir
+		position = position - clone_dir
 		get_parent().add_child(clone)
-		
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_down"):
+		set_linear_velocity(Vector2.DOWN * 110)
+	elif event.is_action_pressed("ui_right"):
+		set_linear_velocity(Vector2.RIGHT * 110)
+	elif event.is_action_pressed("ui_up"):
+		set_linear_velocity(Vector2.UP * 110)
+	elif event.is_action_pressed("ui_left"):
+		set_linear_velocity(Vector2.LEFT * 110)
+	if event.is_action_pressed("ui_accept"):
+		set_linear_velocity(Vector2.ZERO)
+
+
+func _change_velocity():
+	set_linear_velocity(rand_vector() * 110)
+	print("Time to bounce")
